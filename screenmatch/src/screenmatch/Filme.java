4bd14c4daa7d54e33004c5ccb5f5a3d2cb47ba7d
Jose-AGMartins <131082045+Jose-AGMartins@users.noreply.github.com>
2023-08-18 12:01:24 -0300

    Add files via upload

---
+++ screenmatch/src/screenmatch/Filme.java
@@ -0,0 +1,13 @@
+package screenmatch;
+
+public class Filme {
+	String nome;
+	int anoDeLancamento;
+	boolean incluidoNoPlano;
+	double avaliacao;
+	int toalDeAvaliacoes;
+	String resumo;
+	int duracaoEmMinutos;
+	
+
+}
